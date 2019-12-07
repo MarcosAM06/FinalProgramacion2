0ce7765335571062130c2d326e6d466faaeced99
DenisVargas <denis.vargasrivero@outlook.com>
2019-12-07 18:14:52 -0300

    Escena de Prueba para el CCC
    Escena:
    PlayerController + Camera
    Debugs Joystick

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] FixedJoystick _joystick;
+    [SerializeField] float _movementSpeed;
+    [SerializeField] Transform _worldForward;
+
+    [SerializeField] TMP_Text debugText;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+            MovePlayer(_joystick.Horizontal, _joystick.Vertical);
+    }
+
+    public void RotatePlayer(float HAxis)
+    {
+        // Test x Transform
+        //transform.position += transform.forward * MovementSpeed * HAxis * Time.deltaTime;
+    }
+
+    public void MovePlayer(float HAxis, float VAxis)
+    {
+        //Dirección de la cámara.
+        debugText.text = string.Format("Valores del axis son Horizontal: {0} y Vertical: {1}", HAxis, VAxis);
+
+        Vector3 dir = _worldForward.forward * VAxis + _worldForward.right * HAxis;
+        transform.forward = dir;
+        transform.position += dir * _movementSpeed * Time.deltaTime;
     }
 }
