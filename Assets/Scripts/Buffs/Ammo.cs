1100bf0f24fdf8dd8fa5451ec8602650749591d0
DenisVargas <denis.vargasrivero@outlook.com>
2019-12-10 23:19:02 -0300

    Buffs
    Añadidos Buffs:
    1. Heal
    2. Balas
    3. WeaponUpgrade con 4 variaciones posibles.

---
+++ Assets/Scripts/Buffs/Ammo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] WeaponType bulletType = WeaponType.Pistol;
+    [SerializeField] int Ammount = 15;
+    [SerializeField] float _lifeTime = 15f;
+
+    private void Update()
+    {
+        _lifeTime -= Time.deltaTime;
+        if (_lifeTime < 0) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player)
+        {
+            player.AddBullets(bulletType, Ammount);
+
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Buffs/WeaponUpgrade.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class WeaponUpgrade : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] WeaponType weaponType = WeaponType.Pistol;
+    [SerializeField] float _lifeTime = 15f;
+
+    [Header("Buff")]
+    [SerializeField] int _damageBoost = 0;
+    [SerializeField] int _ammoCapacityBoost = 0;
+    [SerializeField, Range(0f, 1f)] float _reloadSpeedBoost = 0f;
+    [SerializeField, Range(0f, 0.5f)] float _fireRateReduction = 0f;
+
+    private void Update()
+    {
+        _lifeTime -= Time.deltaTime;
+        if (_lifeTime < 0) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        Weapon playerWeapon = player.GetWeaponByType(weaponType);
+        if (player && playerWeapon)
+        {
+            playerWeapon.IncreaseDamage(_damageBoost);
+            playerWeapon.IncreaseAmmoCapacity(_ammoCapacityBoost);
+            playerWeapon.IncreaseReloadSpeed(_reloadSpeedBoost);
+            playerWeapon.ReduxFireRate(_fireRateReduction);
+
+            Destroy(gameObject);
+        }
+    }
+}
