ee44ec7a48af62dc7015f3300ce2ac951fb0702c
DenisVargas <denis.vargasrivero@outlook.com>
2019-12-10 23:21:21 -0300

    Interfaces
    Añadido una carpeta para las Interfaces.
    + IFighter
    + IDamageable

---
+++ Assets/Scripts/Interfaces/IFighter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public interface IFighter<TDataInput, TDataOutput>
+{
+    bool IsAlive { get; }
+    Transform transform {get;}
+    GameObject gameObject { get; }
+
+    TDataOutput Hit(TDataInput hitData);
+    TDataInput GetCombatStats();
+    void OnHiConnected(TDataOutput hitResult);
+}
+
+public struct HitData
+{
+    public int Damage;
+}
+
+public struct HitResult
+{
+    public bool Conected;
+}
