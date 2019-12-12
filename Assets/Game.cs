1c6e1af2aad4e5b25526a5784840d2edf0f6c9a9
Marcos Mila <marcos-mila1@hotmail.com>
2019-12-11 21:57:49 -0300

    Boton de pausa

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+
 using UnityEngine.SceneManagement;
+
 
 public enum sceneIndex : uint
 {
@@ -15,6 +17,22 @@ public enum sceneIndex : uint
 
 public class Game : MonoBehaviour
 {
+
+    public CanvasController CC;
+
+    private void Start()
+    {
+        CC = GameObject.Find("Canvas").GetComponent<CanvasController>();
+    }
+
+    void Update()
+    {
+        //Acessos rapidos para los niveles
+
+        CheckButons();
+    }
+
+
     public void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
@@ -26,5 +44,32 @@ public class Game : MonoBehaviour
     public void CloseGame()
     {
         Application.Quit();
+    }
+
+    void CheckButons()
+    {
+
+
+        //if (Input.GetKey(KeyCode.Alpha1))
+        //{
+        //    SceneManager.LoadScene("Level1");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha2))
+        //{
+        //    SceneManager.LoadScene("Level2");
+        //}
+
+        //if (Input.GetKey(KeyCode.Alpha3))
+        //{
+        //    SceneManager.LoadScene("Level3");
+
+        //}
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CC.ActivateOneCanvas("PauseMenu");
+        }
     }
 }
---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour {
+
+    static public CanvasController Instance { get; private set; }
+
+    private GameObject PauseMenu;
+
+    public static bool IsInPause;
+    public List<GameObject> ListCanvasObj = new List<GameObject>();
+
+	void Start ()
+    {
+        Instance = this;
+        FindAllGAmeObjInCanvas();
+    }
+
+    void FindAllGAmeObjInCanvas()
+    {
+        PauseMenu = GameObject.Find("PauseMenu");
+        ListCanvasObj.Add(PauseMenu);
+        PauseMenu.SetActive(false);
+    }
+
+    public void ActivateOneCanvas(string CanvasName)
+    {
+        foreach (var item in ListCanvasObj)
+        {
+            if (item.name != CanvasName)
+            {
+                item.SetActive(false);
+            }
+            else
+            {
+                item.SetActive(true);
+            }
+        }
+                Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        ActivateOneCanvas("Panel");
+        Time.timeScale = 1;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
