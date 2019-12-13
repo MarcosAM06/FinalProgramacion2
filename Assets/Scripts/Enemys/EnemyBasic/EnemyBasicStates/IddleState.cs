60eb92ca7db46e416c32bb4b779c815f65a4be53
Marcos Mila <marcos-mila1@hotmail.com>
2019-12-12 22:38:37 -0300

    Estados enemy basic

--- Assets/Scripts/Enemys/EnemyBasic/EnemyBasicStates/IddleState.cs
+++ Assets/Scripts/Enemys/EnemyBasic/EnemyBasicStates/IddleState.cs
@@ -5,9 +5,35 @@ using UnityEngine;
 public class IddleState<T> : State<T>
 {
 
+    public Animator Enemybasic;
+    public EnemyBasic Enemy;
+
+
+
+    public IddleState(EnemyBasic enemyBasic, Animator enemyBasicAnim)
+    {
+        this.Enemybasic = enemyBasicAnim;
+        Enemy = enemyBasic;
+
+    }
+
+
     public override void Enter()
     {
         Debug.Log("Enemigo iddle");
+        Enemybasic.SetBool("IsWalking", false);
     }
+
+    public override void Update()
+    {
+        if (Enemy.onDamage == true)
+            {
+                Enemy.stateMachine.Feed(EnemyBasic.Feed.TakingDamage);
+            }
+    }
+
+      
+
+
 
 }
