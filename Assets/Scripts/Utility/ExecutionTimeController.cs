a00394dca6d869bee96bed660e8d6ef2f64d3465
DenisVargas <denis.vargasrivero@outlook.com>
2019-12-13 17:02:29 -0300

    Añadido una Utilidad
    Directorio nuevo: "Utility"
    Script ExecutionTimeController, que permite reducir la escala del tiempo para chequear cosas en playMode.

---
+++ Assets/Scripts/Utility/ExecutionTimeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ExecutionTimeController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+            ChangeTimeScale(0f);
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            ChangeTimeScale(1f);
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            ChangeTimeScale(0.1f);
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            ChangeTimeScale(0.2f);
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+            ChangeTimeScale(0.3f);
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+            ChangeTimeScale(0.4f);
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+            ChangeTimeScale(0.5f);
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+            ChangeTimeScale(0.6f);
+        if (Input.GetKeyDown(KeyCode.Keypad7))
+            ChangeTimeScale(0.7f);
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+            ChangeTimeScale(0.8f);
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+            ChangeTimeScale(0.9f);
+    }
+
+    public void ChangeTimeScale(float Ammount)
+    {
+        Time.timeScale = Ammount;
+        Debug.LogWarning("La escala de tiempo ha sido modificada a :" + Time.timeScale);
+    }
+}
