125c3716a2b1e9a825ae26fe5028a122ffe655d7
DenisVargas <denis.vargasrivero@outlook.com>
2019-12-20 07:40:19 -0300

    ProgressTracker :100:

---
+++ Assets/Scripts/ArchivementData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ArchievementsUnlocked", menuName = "DataHolder", order = 0)]
+public class ArchivementData : ScriptableObject
+{
+    public bool Nivel1Completado = false;
+    public bool Nivel2Completado = false;
+    public bool JuegoCompletado = false;
+    public bool BossAsesinado = false;
+    public bool EnemigoSimplesAsesinados = false;
+    public bool EnemigosRangoAsesinados = false;
+
+    public int TotalEnemigosSimplesAsesinados = 0;
+    public int TotalEnemigosRangoAsesinados = 0;
+    public int TotalBossesEliminados = 0;
+
+    public bool PrimeraMuerte = false;
+}
---
+++ Assets/Scripts/GameProgressTracker.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class GameProgressTracker : MonoBehaviour
+{
+    public static GameProgressTracker instance;
+
+    public const string DataPath = "Archievements";
+    public ArchivementData unlockedArchivements;
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+        else Destroy(this);
+
+        LoadArchivementData();
+    }
+
+    public void LoadArchivementData()
+    {
+        unlockedArchivements = Resources.Load<ArchivementData>("Archievements");
+    }
+
+    public static void NotifyCompletedLevel(sceneIndex level)
+    {
+        if (instance.unlockedArchivements == null)
+            instance.LoadArchivementData();
+
+        switch (level)
+        {
+            case sceneIndex.Lvl1:
+                instance.unlockedArchivements.Nivel1Completado = true;
+                break;
+            case sceneIndex.Lvl2:
+                instance.unlockedArchivements.Nivel2Completado = true;
+                break;
+            case sceneIndex.Lvl3:
+                instance.unlockedArchivements.JuegoCompletado = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void NofitySimpleEnemyKilled()
+    {
+        if (instance.unlockedArchivements == null)
+            instance.LoadArchivementData();
+
+        instance.unlockedArchivements.TotalEnemigosSimplesAsesinados++;
+
+        if (instance.unlockedArchivements.TotalEnemigosSimplesAsesinados == 5)
+            instance.unlockedArchivements.EnemigoSimplesAsesinados = true;
+
+        //LLamamos a los eventos de Analitics.
+    }
+
+    public static void NotifyRangeEnemyKilled()
+    {
+        if (instance.unlockedArchivements == null)
+            instance.LoadArchivementData();
+
+        instance.unlockedArchivements.TotalEnemigosRangoAsesinados++;
+
+        if (instance.unlockedArchivements.TotalEnemigosRangoAsesinados == 5)
+            instance.unlockedArchivements.EnemigosRangoAsesinados = true;
+
+        //LLamamos a Analitics again.
+    }
+
+    public static void NotifyPlayerDied()
+    {
+        if (instance.unlockedArchivements == null)
+            instance.LoadArchivementData();
+
+        if (!instance.unlockedArchivements.PrimeraMuerte)
+        {
+            instance.unlockedArchivements.PrimeraMuerte = true;
+            //Notificamos a Analitics.
+        }
+
+        //Notificacion doble.
+    }
+
+    public static void BossEnemyKilled()
+    {
+        if (instance.unlockedArchivements == null)
+            instance.LoadArchivementData();
+
+        if (!instance.unlockedArchivements.BossAsesinado)
+        {
+            instance.unlockedArchivements.BossAsesinado = true;
+            //Notificación.
+        }
+
+        instance.unlockedArchivements.TotalBossesEliminados++;
+        //Notificación Analitics Final.
+    }
+}
